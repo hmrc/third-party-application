2eb2e5a672349f3d0e21ac948b6ac4bae1dd40df
Chris Rowe <chris@zeroalpha.technology>
2019-08-20 11:44:51 +0100

    APSR-1100: Log Rate Limit Tier mismatches between TPA and WSO2

---
+++ app/uk/gov/hmrc/thirdpartyapplication/scheduled/ReconcileRateLimitsScheduledJob.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.thirdpartyapplication.scheduled
+
+import javax.inject.Inject
+import org.joda.time.Duration
+import play.api.{Logger, LoggerLike}
+import play.modules.reactivemongo.ReactiveMongoComponent
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.lock.{LockKeeper, LockRepository}
+import uk.gov.hmrc.thirdpartyapplication.connector.Wso2ApiStoreConnector
+import uk.gov.hmrc.thirdpartyapplication.models.db.ApplicationData
+import uk.gov.hmrc.thirdpartyapplication.repository.ApplicationRepository
+
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ExecutionContext, Future}
+
+class ReconcileRateLimitsScheduledJob @Inject()(val lockKeeper: ReconcileRateLimitsJobLockKeeper,
+                                                applicationRepository: ApplicationRepository,
+                                                wso2ApiStoreConnector: Wso2ApiStoreConnector,
+                                                jobConfig: ReconcileRateLimitsJobConfig,
+                                                logger: LoggerLike = Logger) extends ScheduledMongoJob {
+
+  override def name: String = "ReconcileRateLimitsScheduledJob"
+  override def interval: FiniteDuration = jobConfig.interval
+  override def initialDelay: FiniteDuration = jobConfig.initialDelay
+
+  override def runJob(implicit ec: ExecutionContext): Future[RunningOfJobSuccessful] = {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+
+    logger.info(s"Starting Rate Limit Reconciliation Job")
+
+    for {
+      cookie <- wso2ApiStoreConnector.login("", "")
+      _ <- applicationRepository.processAll(tpaApplication => reconcileApplicationRateLimit(tpaApplication, cookie))
+      _ <- wso2ApiStoreConnector.logout(cookie)
+    } yield RunningOfJobSuccessful
+  }
+
+  def reconcileApplicationRateLimit(tpaApplication: ApplicationData, wso2Cookie: String)
+                                         (implicit ec: ExecutionContext, headerCarrier: HeaderCarrier): Future[Unit] = {
+    wso2ApiStoreConnector.getApplicationRateLimitTier(wso2Cookie, tpaApplication.wso2ApplicationName) map {
+      wso2ApplicationRateLimit =>
+        tpaApplication.rateLimitTier match {
+          case None =>
+            logger.warn(s"No Rate Limit stored in TPA for Application [${tpaApplication.wso2ApplicationName}]. WSO2 Rate Limit is [$wso2ApplicationRateLimit]")
+          case Some(tpaApplicationRateLimit) =>
+            if (tpaApplicationRateLimit == wso2ApplicationRateLimit) {
+              logger.debug(s"Rate Limits in TPA and WSO2 match for Application [${tpaApplication.wso2ApplicationName}].")
+            } else {
+              logger.warn(
+                s"Rate Limit mismatch for Application [${tpaApplication.wso2ApplicationName}]. " +
+                  s"TPA: [${tpaApplication.rateLimitTier.get}], WSO2: [$wso2ApplicationRateLimit]")
+            }
+        }
+    }
+  }
+}
+
+class ReconcileRateLimitsJobLockKeeper @Inject()(mongo: ReactiveMongoComponent) extends LockKeeper {
+  override def repo: LockRepository = new LockRepository()(mongo.mongoConnector.db)
+
+  override def lockId: String = "ReconcileRateLimitsScheduledJob"
+
+  override val forceLockReleaseAfter: Duration = Duration.standardHours(2)
+
+}
+
+case class ReconcileRateLimitsJobConfig(initialDelay: FiniteDuration, interval: FiniteDuration, enabled: Boolean)
---
+++ test/unit/uk/gov/hmrc/thirdpartyapplication/scheduled/ReconcileRateLimitsScheduledJobSpec.scala
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.thirdpartyapplication.scheduled
+
+import java.util.UUID
+import java.util.concurrent.TimeUnit.{DAYS, SECONDS}
+
+import common.uk.gov.hmrc.thirdpartyapplication.testutils.ApplicationStateUtil
+import org.joda.time.{DateTime, Duration}
+import org.mockito.Matchers.{any, matches}
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.mockito.MockitoSugar
+import org.slf4j
+import play.api.LoggerLike
+import play.modules.reactivemongo.ReactiveMongoComponent
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.lock.LockRepository
+import uk.gov.hmrc.mongo.{MongoConnector, MongoSpecSupport}
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.thirdpartyapplication.connector.Wso2ApiStoreConnector
+import uk.gov.hmrc.thirdpartyapplication.models.RateLimitTier.RateLimitTier
+import uk.gov.hmrc.thirdpartyapplication.models.db.{ApplicationData, ApplicationTokens}
+import uk.gov.hmrc.thirdpartyapplication.models.{ApplicationState, EnvironmentToken, HasSucceeded, RateLimitTier}
+import uk.gov.hmrc.thirdpartyapplication.repository.ApplicationRepository
+import uk.gov.hmrc.thirdpartyapplication.scheduled.{ReconcileRateLimitsJobConfig, ReconcileRateLimitsJobLockKeeper, ReconcileRateLimitsScheduledJob}
+import uk.gov.hmrc.time.{DateTimeUtils => HmrcTime}
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ExecutionContext, Future}
+
+class ReconcileRateLimitsScheduledJobSpec extends UnitSpec with MockitoSugar with MongoSpecSupport with BeforeAndAfterAll with ApplicationStateUtil {
+
+  val FixedTimeNow: DateTime = HmrcTime.now
+  val expiryTimeInDays = 90
+
+  class StubLogger extends LoggerLike {
+    override val logger: slf4j.Logger = mock[slf4j.Logger]
+
+    val infoMessages = new ListBuffer[String]()
+    val debugMessages = new ListBuffer[String]()
+    val warnMessages = new ListBuffer[String]()
+
+    override def info(message: => String): Unit = infoMessages += message
+    override def debug(message: => String): Unit = debugMessages += message
+    override def warn(message: => String): Unit = warnMessages += message
+  }
+
+  trait Setup {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+
+    val lockKeeperSuccess: () => Boolean = () => true
+
+    private val reactiveMongoComponent = new ReactiveMongoComponent {
+      override def mongoConnector: MongoConnector = mongoConnectorForTest
+    }
+
+    val mockLockKeeper: ReconcileRateLimitsJobLockKeeper = new ReconcileRateLimitsJobLockKeeper(reactiveMongoComponent) {
+      override def lockId: String = "reconcileRateLimitsTestLock"
+
+      override def repo: LockRepository = mock[LockRepository]
+
+      override val forceLockReleaseAfter: Duration = Duration.standardMinutes(5) // scalastyle:off magic.number
+
+      override def tryLock[T](body: => Future[T])(implicit ec: ExecutionContext): Future[Option[T]] =
+        if (lockKeeperSuccess()) body.map(value => Future.successful(Some(value)))
+        else Future.successful(None)
+    }
+
+    def testApplication(wso2ApplicationName: String, rateLimit: Option[RateLimitTier]): ApplicationData =
+      new ApplicationData(
+        id = UUID.randomUUID(),
+        name = "",
+        normalisedName = "",
+        collaborators = Set.empty,
+        wso2Username = "",
+        wso2Password = "",
+        wso2ApplicationName = wso2ApplicationName,
+        tokens = ApplicationTokens(new EnvironmentToken("", "", "")),
+        state = ApplicationState(),
+        rateLimitTier = rateLimit)
+
+    val mockApplicationRepository: ApplicationRepository = mock[ApplicationRepository]
+    val mockWSO2ApiStoreConnector: Wso2ApiStoreConnector = mock[Wso2ApiStoreConnector]
+
+    val stubLogger = new StubLogger
+
+    val config = ReconcileRateLimitsJobConfig(FiniteDuration(120, SECONDS), FiniteDuration(60, DAYS), enabled = true) // scalastyle:off magic.number
+    val underTest = new ReconcileRateLimitsScheduledJob(mockLockKeeper, mockApplicationRepository, mockWSO2ApiStoreConnector, config, stubLogger)
+  }
+
+  "processJob" should {
+    "create session in WSO2 to retrieve API information" in new Setup {
+      val wso2Cookie: String = UUID.randomUUID().toString
+      when(mockWSO2ApiStoreConnector.login(matches(""), matches(""))(any[HeaderCarrier])).thenReturn(Future.successful(wso2Cookie))
+      when(mockApplicationRepository.processAll(any())).thenReturn(Future.successful())
+      when(mockWSO2ApiStoreConnector.logout(matches(wso2Cookie))(any[HeaderCarrier])).thenReturn(Future.successful(HasSucceeded))
+
+      val result = await(underTest.runJob)
+
+      verify(mockWSO2ApiStoreConnector).login(matches(""), matches(""))(any[HeaderCarrier])
+      verify(mockApplicationRepository).processAll(any())
+      verify(mockWSO2ApiStoreConnector).logout(matches(wso2Cookie))(any[HeaderCarrier])
+    }
+  }
+
+  "reconcileApplicationRateLimit" should {
+    val wso2Cookie: String = UUID.randomUUID().toString
+    val wso2ApplicationName = "foo"
+
+    "log at DEBUG when Rate Limits match" in new Setup {
+      val wso2RateLimit: RateLimitTier = RateLimitTier.SILVER
+      val tpaRateLimit: RateLimitTier = RateLimitTier.SILVER
+
+      val application: ApplicationData = testApplication(wso2ApplicationName, Some(tpaRateLimit))
+      val expectedMessage = s"Rate Limits in TPA and WSO2 match for Application [$wso2ApplicationName]."
+
+      when(mockWSO2ApiStoreConnector.getApplicationRateLimitTier(matches(wso2Cookie), matches(wso2ApplicationName))(any[HeaderCarrier]))
+        .thenReturn(Future.successful(wso2RateLimit))
+
+      await(underTest.reconcileApplicationRateLimit(application, wso2Cookie))
+
+      stubLogger.debugMessages.size should be (1)
+      stubLogger.debugMessages.toList.head should be (expectedMessage)
+    }
+
+    "log at WARN when Rate Limits do not match" in new Setup {
+      val wso2RateLimit: RateLimitTier = RateLimitTier.SILVER
+      val tpaRateLimit: RateLimitTier = RateLimitTier.GOLD
+
+      val application: ApplicationData = testApplication(wso2ApplicationName, Some(tpaRateLimit))
+      val expectedMessage = s"Rate Limit mismatch for Application [$wso2ApplicationName]. TPA: [$tpaRateLimit], WSO2: [$wso2RateLimit]"
+
+      when(mockWSO2ApiStoreConnector.getApplicationRateLimitTier(matches(wso2Cookie), matches(wso2ApplicationName))(any[HeaderCarrier]))
+        .thenReturn(Future.successful(wso2RateLimit))
+
+      await(underTest.reconcileApplicationRateLimit(application, wso2Cookie))
+
+      stubLogger.warnMessages.size should be (1)
+      stubLogger.warnMessages.toList.head should be (expectedMessage)
+    }
+
+    "handle case where TPA does not have a Rate Limit assigned" in new Setup {
+      val wso2RateLimit: RateLimitTier = RateLimitTier.SILVER
+
+      val application: ApplicationData = testApplication(wso2ApplicationName, None)
+      val expectedMessage = s"No Rate Limit stored in TPA for Application [$wso2ApplicationName]. WSO2 Rate Limit is [$wso2RateLimit]"
+
+      when(mockWSO2ApiStoreConnector.getApplicationRateLimitTier(matches(wso2Cookie), matches(wso2ApplicationName))(any[HeaderCarrier]))
+        .thenReturn(Future.successful(wso2RateLimit))
+
+      await(underTest.reconcileApplicationRateLimit(application, wso2Cookie))
+
+      stubLogger.warnMessages.size should be (1)
+      stubLogger.warnMessages.toList.head should be (expectedMessage)
+    }
+  }
+}
