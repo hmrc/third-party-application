467bc90f5b740ac4015ad4d348ad848f8e3256cf
Adam Pridmore <adam_pridmore@hotmail.com>
2019-12-13 11:46:07 +0000

    APSR-1161 - Made Metrics use the ScheduledMongoJob  - Instead of the ActorSystem

--- app/uk/gov/hmrc/thirdpartyapplication/metrics/MetricsModule.scala
+++ app/uk/gov/hmrc/thirdpartyapplication/metrics/MetricsModule.scala
@@ -16,12 +16,11 @@
 
 package uk.gov.hmrc.thirdpartyapplication.metrics
 
-import akka.actor.ActorSystem
 import com.kenshoo.play.metrics.Metrics
 import javax.inject.{Inject, Provider, Singleton}
 import org.joda.time.Duration
-import play.api.inject.{ApplicationLifecycle, Binding, Module}
-import play.api.{Configuration, Environment, Logger}
+import play.api.inject.{Binding, Module}
+import play.api.{Configuration, Environment}
 import play.modules.reactivemongo.ReactiveMongoComponent
 import reactivemongo.api.DB
 import uk.gov.hmrc.lock.{ExclusiveTimePeriodLock, LockRepository}
@@ -29,41 +28,14 @@ import uk.gov.hmrc.metrix.MetricOrchestrator
 import uk.gov.hmrc.metrix.domain.MetricSource
 import uk.gov.hmrc.metrix.persistence.MongoMetricRepository
 
-import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
 class MetricsModule extends Module {
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
     Seq(
       bind[MetricOrchestrator].toProvider[MetricsOrchestratorProvider],
-      bind[MetricsSources].toProvider[MetricsSourcesProvider],
-      bind[MetricsScheduler].toSelf.eagerly)
+      bind[MetricsSources].toProvider[MetricsSourcesProvider])
   }
-}
-
-@Singleton
-class MetricsScheduler @Inject()(env: Environment,
-                                 lifecycle: ApplicationLifecycle,
-                                 actorSystem: ActorSystem,
-                                 configuration: Configuration,
-                                 metricOrchestrator: MetricOrchestrator)(implicit val ec: ExecutionContext) {
-  private val initialDelay = 2 minutes
-  private val interval = 1 hour
-
-  Logger.info(s"Configuring Metrics schedule to run every $interval")
-
-  actorSystem.scheduler.schedule(initialDelay, interval) {
-    Logger.info(s"Running Metrics Collection Process")
-    metricOrchestrator
-      .attemptToUpdateAndRefreshMetrics()
-      .map(_.andLogTheResult())
-      .recover { case e: RuntimeException => Logger.error(s"An error occurred processing metrics: ${e.getMessage}", e) }
-  }
-
-  lifecycle.addStopHook(() => Future {
-    actorSystem.terminate()
-  })
 }
 
 @Singleton
