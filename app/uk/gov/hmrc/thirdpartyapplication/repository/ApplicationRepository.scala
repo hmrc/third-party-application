e485982bcb243bbb95209fbc7c715cf0b64b858c
Chris Rowe <chris@zeroalpha.technology>
2020-04-08 11:27:12 +0100

    API-4270: Add function to update Client Secret name

--- app/uk/gov/hmrc/thirdpartyapplication/repository/ApplicationRepository.scala
+++ app/uk/gov/hmrc/thirdpartyapplication/repository/ApplicationRepository.scala
@@ -142,6 +142,13 @@ class ApplicationRepository @Inject()(mongo: ReactiveMongoComponent)(implicit va
   def addClientSecret(applicationId: UUID, clientSecret: ClientSecret): Future[ApplicationData] =
     updateApplication(applicationId, Json.obj("$push" -> Json.obj("tokens.production.clientSecrets" -> Json.toJson(clientSecret))))
 
+  def updateClientSecretName(applicationId: UUID, clientSecretId: String, newName: String): Future[ApplicationData] =
+    findAndUpdate(
+      Json.obj("id" -> applicationId.toString, "tokens.production.clientSecrets.id" -> clientSecretId),
+      Json.obj("$set" -> Json.obj("tokens.production.clientSecrets.$.name" -> newName)),
+      fetchNewObject = true)
+      .map(_.result[ApplicationData].head)
+
   def updateClientSecretHash(applicationId: UUID, clientSecretId: String, hashedSecret: String): Future[ApplicationData] =
     findAndUpdate(
       Json.obj("id" -> applicationId.toString, "tokens.production.clientSecrets.id" -> clientSecretId),
