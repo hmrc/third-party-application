1cc3347f198352f94287a3ed5836883c200f622d
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2022-05-06 15:24:59 +0100

    APIS-5779 Responsible individual accept and decline

---
+++ app/uk/gov/hmrc/apiplatform/modules/approvals/controllers/ResponsibleIndividualVerificationController.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.approvals.controllers
+
+import play.api.mvc.ControllerComponents
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import uk.gov.hmrc.thirdpartyapplication.controllers.JsonUtils
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.apiplatform.modules.approvals.domain.models.ResponsibleIndividualVerification
+import play.api.libs.json.Json
+import play.api.mvc.Results
+import uk.gov.hmrc.thirdpartyapplication.models.JsonFormatters._
+
+import scala.concurrent.ExecutionContext
+import uk.gov.hmrc.apiplatform.modules.approvals.services.ResponsibleIndividualVerificationService
+import uk.gov.hmrc.apiplatform.modules.approvals.controllers.actions.JsonErrorResponse
+
+object ResponsibleIndividualVerificationController {
+  case class ResponsibleIndividualVerificationRequest(code: String) 
+  implicit val readsResponsibleIndividualVerificationRequest = Json.reads[ResponsibleIndividualVerificationRequest]
+
+  case class ErrorMessage(message: String)
+  implicit val writesErrorMessage = Json.writes[ErrorMessage]
+}
+
+@Singleton
+class ResponsibleIndividualVerificationController @Inject()(
+  val responsibleIndividualVerificationService: ResponsibleIndividualVerificationService,
+  cc: ControllerComponents
+)
+(implicit val ec: ExecutionContext) extends BackendController(cc)
+    with JsonUtils
+    with JsonErrorResponse {
+
+  import ResponsibleIndividualVerificationController._
+
+  def getVerification(code: String) = Action.async { implicit request =>
+
+    lazy val failed = NotFound(Results.EmptyContent())
+    val success = (responsibleIndividualVerification: ResponsibleIndividualVerification) => Ok(Json.toJson(responsibleIndividualVerification))
+
+    responsibleIndividualVerificationService.getVerification(code).map(_.fold(failed)(success))
+  }
+
+  def accept() = Action.async { implicit request =>
+
+    val failed = (msg: String) => BadRequest(Json.toJson(ErrorMessage(msg)))
+    val success = (responsibleIndividualVerification: ResponsibleIndividualVerification) => Ok(Json.toJson(responsibleIndividualVerification))
+
+    withJsonBodyFromAnyContent[ResponsibleIndividualVerificationRequest] { riVerificationRequest =>
+      responsibleIndividualVerificationService.accept(riVerificationRequest.code).map(_.fold(failed, success))
+    }
+  }
+
+  def decline() = Action.async { implicit request =>
+
+    val failed = (msg: String) => BadRequest(Json.toJson(ErrorMessage(msg)))
+    val success = (responsibleIndividualVerification: ResponsibleIndividualVerification) => Ok(Json.toJson(responsibleIndividualVerification))
+
+    withJsonBodyFromAnyContent[ResponsibleIndividualVerificationRequest] { riVerificationRequest =>
+      responsibleIndividualVerificationService.decline(riVerificationRequest.code).map(_.fold(failed, success))
+    }
+  }
+}
--- app/uk/gov/hmrc/apiplatform/modules/approvals/services/ResponsibleIndividualVerificationService.scala
+++ app/uk/gov/hmrc/apiplatform/modules/approvals/services/ResponsibleIndividualVerificationService.scala
@@ -17,14 +17,18 @@
 package uk.gov.hmrc.apiplatform.modules.approvals.services
 
 import uk.gov.hmrc.apiplatform.modules.approvals.domain.models.ResponsibleIndividualVerification
+import uk.gov.hmrc.apiplatform.modules.approvals.domain.models.ResponsibleIndividualVerificationId
 import uk.gov.hmrc.apiplatform.modules.approvals.repositories.ResponsibleIndividualVerificationDAO
 import uk.gov.hmrc.apiplatform.modules.submissions.domain.models.Submission
 import uk.gov.hmrc.thirdpartyapplication.models.db.ApplicationData
-
+import uk.gov.hmrc.apiplatform.modules.common.services.EitherTHelper
+import scala.concurrent.{ExecutionContext, Future}
 import javax.inject.Inject
 
 class ResponsibleIndividualVerificationService @Inject()(
-    responsibleIndividualVerificationDao: ResponsibleIndividualVerificationDAO) {
+    responsibleIndividualVerificationDao: ResponsibleIndividualVerificationDAO
+  )(implicit ec: ExecutionContext) {
+
   def createNewVerification(applicationData: ApplicationData, submissionId: Submission.Id, submissionInstance: Int) = {
     val verification = ResponsibleIndividualVerification(
       applicationId = applicationData.id,
@@ -33,5 +37,37 @@ class ResponsibleIndividualVerificationService @Inject()(
       applicationName = applicationData.name
     )
     responsibleIndividualVerificationDao.save(verification)
+  }
+
+  def getVerification(code: String): Future[Option[ResponsibleIndividualVerification]] = {
+    responsibleIndividualVerificationDao.fetch(ResponsibleIndividualVerificationId(code))
+  }
+
+  def accept(code: String): Future[Either[String, ResponsibleIndividualVerification]] = {
+    import cats.implicits._
+    import cats.instances.future.catsStdInstancesForFuture
+
+    val ET = EitherTHelper.make[String]
+
+    // TODO: Change state of application to PENDING_GATEKEEPER_APPROVAL and save timestamp. Also delete verification record.
+    (
+      for {
+        riVerification <- ET.fromOptionF(responsibleIndividualVerificationDao.fetch(ResponsibleIndividualVerificationId(code)), "responsibleIndividualVerification not found")
+      } yield riVerification
+    ).value
+  }
+
+  def decline(code: String): Future[Either[String, ResponsibleIndividualVerification]] = {
+    import cats.implicits._
+    import cats.instances.future.catsStdInstancesForFuture
+
+    val ET = EitherTHelper.make[String]
+
+    // TODO: Decline the request, with an appropriate reason. Also delete verification record.
+    (
+      for {
+        riVerification <- ET.fromOptionF(responsibleIndividualVerificationDao.fetch(ResponsibleIndividualVerificationId(code)), "responsibleIndividualVerification not found")
+      } yield riVerification
+    ).value
   }
 }
