d371cd5c5161d1a513a97e73aa1e02c64efb48a1
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2022-12-19 13:03:23 +0000

    API-6287 PR actions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,15 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
+  lazy val bootstrapVersion = "5.16.0"
+  lazy val hmrcMongoVersion = "0.70.0"
+
   private lazy val compileDeps = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"         % "5.16.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"         % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-json-union-formatter"         % "1.15.0-play-28",
     "com.typesafe.play" %% "play-json"                         % "2.9.2",
     "uk.gov.hmrc"       %% "play-hmrc-api"                     % "6.4.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-28" % "0.70.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongoVersion,
     "commons-net"        % "commons-net"                       % "3.6",
     "org.typelevel"     %% "cats-core"                         % "2.0.0",
     "com.github.t3hnar" %% "scala-bcrypt"                      % "4.1",
@@ -19,8 +22,8 @@ object AppDependencies {
   )
 
   private lazy val testDeps = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-test-play-28"   % "5.16.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-28"  % "0.64.0",
+    "uk.gov.hmrc"           %% "bootstrap-test-play-28"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
     "org.scalaj"            %% "scalaj-http"              % "2.3.0",
     "com.github.tomakehurst" % "wiremock-jre8-standalone" % "2.27.2",
     "org.mockito"           %% "mockito-scala-scalatest"  % "1.16.42"
