c1260b34fd59947071ade96f6369f8aa36a13808
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-07-18 16:13:31 +0100

    API-7200 - Remove thread pool that went in by mistake

--- test/uk/gov/hmrc/thirdpartyapplication/services/ClientSecretServiceSpec.scala
+++ test/uk/gov/hmrc/thirdpartyapplication/services/ClientSecretServiceSpec.scala
@@ -25,6 +25,7 @@ import uk.gov.hmrc.thirdpartyapplication.domain.models.ClientSecretData
 import uk.gov.hmrc.thirdpartyapplication.mocks.repository.ApplicationRepositoryMockModule
 import uk.gov.hmrc.thirdpartyapplication.models.db.ApplicationData
 import uk.gov.hmrc.thirdpartyapplication.util.AsyncHmrcSpec
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ClientSecretServiceSpec extends AsyncHmrcSpec with FixedClock {
 
