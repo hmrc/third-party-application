c10a0b12dd623a9624c49e8eddf161fac28e1111
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-07-02 20:32:02 +0100

    APIS-7019 Add command handler for BlockApplication

---
+++ app/uk/gov/hmrc/thirdpartyapplication/services/commands/block/BlockApplicationCommandHandler.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.thirdpartyapplication.services.commands.block
+
+import java.time.Clock
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+import cats.data.{NonEmptyList, Validated}
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.Actors
+import uk.gov.hmrc.apiplatform.modules.common.services.{ApplicationLogger, ClockNow}
+import uk.gov.hmrc.apiplatform.modules.commands.applications.domain.models.ApplicationCommands.BlockApplication
+import uk.gov.hmrc.apiplatform.modules.events.applications.domain.models.ApplicationEvents._
+import uk.gov.hmrc.apiplatform.modules.events.applications.domain.models._
+import uk.gov.hmrc.thirdpartyapplication.models.db.StoredApplication
+import uk.gov.hmrc.thirdpartyapplication.repository.ApplicationRepository
+import uk.gov.hmrc.thirdpartyapplication.services.commands.CommandHandler
+
+@Singleton
+class BlockApplicationCommandHandler @Inject() (
+    applicationRepository: ApplicationRepository,
+    val clock: Clock
+  )(implicit val ec: ExecutionContext
+  ) extends CommandHandler with ApplicationLogger with ClockNow {
+
+  import CommandHandler._
+
+  private def validate(app: StoredApplication): Validated[Failures, StoredApplication] = {
+    Validated.validNel(app)
+  }
+
+  private def asEvents(
+      app: StoredApplication,
+      cmd: BlockApplication
+    ): NonEmptyList[ApplicationEvent] = {
+
+    NonEmptyList.of(ApplicationBlocked(
+      id = EventId.random,
+      applicationId = app.id,
+      eventDateTime = cmd.timestamp,
+      actor = Actors.GatekeeperUser(cmd.gatekeeperUser)
+    ))
+  }
+
+  def process(app: StoredApplication, cmd: BlockApplication): AppCmdResultT = {
+    def block(application: StoredApplication): StoredApplication = {
+      application.copy(blocked = true)
+    }
+
+    for {
+      valid <- E.fromEither(validate(app).toEither)
+      _     <- E.liftF(applicationRepository.save(block(app)))
+      events = asEvents(app, cmd)
+    } yield (app, events)
+  }
+}
---
+++ test/uk/gov/hmrc/thirdpartyapplication/services/commands/block/BlockApplicationCommandHandlerSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.thirdpartyapplication.services.commands.block
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import uk.gov.hmrc.http.HeaderCarrier
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.{Actors, ApplicationId, Environment}
+import uk.gov.hmrc.apiplatform.modules.common.utils.FixedClock
+import uk.gov.hmrc.apiplatform.modules.commands.applications.domain.models.ApplicationCommands.BlockApplication
+import uk.gov.hmrc.apiplatform.modules.events.applications.domain.models.ApplicationEvents.ApplicationBlocked
+import uk.gov.hmrc.thirdpartyapplication.mocks.repository.ApplicationRepositoryMockModule
+import uk.gov.hmrc.thirdpartyapplication.services.commands.CommandHandlerBaseSpec
+
+class BlockApplicationCommandHandlerSpec extends CommandHandlerBaseSpec {
+
+  trait Setup extends ApplicationRepositoryMockModule {
+
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+
+    val appId = ApplicationId.random
+    val app   = anApplicationData(appId, environment = Environment.SANDBOX)
+    val ts    = FixedClock.instant
+
+    val underTest = new BlockApplicationCommandHandler(
+      ApplicationRepoMock.aMock,
+      FixedClock.clock
+    )
+  }
+
+  "BlockApplication" should {
+
+    "succeed as gkUserActor" in new Setup {
+      ApplicationRepoMock.Save.thenReturn(app)
+
+      val cmd = BlockApplication(gatekeeperUser, ts)
+
+      val result = await(underTest.process(app, cmd).value).value
+
+      inside(result) { case (_, events) =>
+        events should have size 1
+
+        inside(events.head) {
+          case event: ApplicationBlocked =>
+            event.applicationId shouldBe appId
+            event.eventDateTime shouldBe ts
+            event.actor shouldBe Actors.GatekeeperUser(gatekeeperUser)
+        }
+      }
+    }
+  }
+}
