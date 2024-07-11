64453a4d6b42a057cc9d98ee487f55c610de9246
Pete Slater <28756214+peteslater-ee@users.noreply.github.com>
2024-07-11 08:33:53 +0100

    APIS-7186 - Fixing test issues for Java 21

--- it/test/uk/gov/hmrc/thirdpartyapplication/MyTestServerFactory.scala
+++ it/test/uk/gov/hmrc/thirdpartyapplication/MyTestServerFactory.scala
@@ -25,7 +25,7 @@ object MyTestServerFactory extends MyTestServer
 class MyTestServer extends DefaultTestServerFactory {
 
   override protected def serverConfig(app: Application): ServerConfig = {
-    val sc = ServerConfig(port = Some(6001), sslPort = Some(6002), mode = Mode.Test, rootDir = app.path)
+    val sc = ServerConfig(port = Some(6001), sslPort = None, mode = Mode.Test, rootDir = app.path)
     sc.copy(configuration = sc.configuration withFallback overrideServerConfiguration(app))
   }
 }
