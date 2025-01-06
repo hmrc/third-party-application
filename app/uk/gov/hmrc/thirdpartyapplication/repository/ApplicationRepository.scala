e6e7cb4690c78befb0f765ea51f721a0a160ae6e
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-01-06 11:31:37 +0000

    APIS-7389 Add new search filter for delete restriction

--- app/uk/gov/hmrc/thirdpartyapplication/repository/ApplicationRepository.scala
+++ app/uk/gov/hmrc/thirdpartyapplication/repository/ApplicationRepository.scala
@@ -611,7 +611,14 @@ class ApplicationRepository @Inject() (mongo: MongoComponent, val metrics: Metri
         case false => matches(equal("allowAutoDelete", Codecs.toBson(allowAutoDelete)))
         case true  => matches(or(equal("allowAutoDelete", Codecs.toBson(allowAutoDelete)), exists("allowAutoDelete", false)))
       }
+    }
 
+    def deleteRestrictionMatch(restrictionType: DeleteRestrictionType): Bson = {
+      restrictionType match {
+        case DeleteRestrictionType.DO_NOT_DELETE  => matches(equal("deleteRestriction.deleteRestrictionType", Codecs.toBson(restrictionType)))
+        case DeleteRestrictionType.NO_RESTRICTION =>
+          matches(or(equal("deleteRestriction.deleteRestrictionType", Codecs.toBson(restrictionType)), exists("deleteRestriction.deleteRestrictionType", false)))
+      }
     }
 
     def specificAPISubscription(apiContext: ApiContext, apiVersion: Option[ApiVersionNbr]) = {
@@ -659,7 +666,12 @@ class ApplicationRepository @Inject() (mongo: MongoComponent, val metrics: Metri
       // Allow Auto Delete
       case AutoDeleteAllowed => allowAutoDeleteMatch(true)
       case AutoDeleteBlocked => allowAutoDeleteMatch(false)
-      case _                 => Document() // Only here to complete the match
+
+      // Delete Restriction
+      case NoRestriction => deleteRestrictionMatch(DeleteRestrictionType.NO_RESTRICTION)
+      case DoNotDelete   => deleteRestrictionMatch(DeleteRestrictionType.DO_NOT_DELETE)
+
+      case _ => Document() // Only here to complete the match
     }
   }
   // scalastyle:on cyclomatic.complexity
