3f83be3324e2bd0429f39a5d48fae93dc9424c32
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-08-27 16:16:43 +0100

    APIS-7737 PR bot actions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import org.apache.ivy.core.module.descriptor.ExcludeRule
 object AppDependencies {
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
-  lazy val bootstrapVersion         = "9.13.0"
+  lazy val bootstrapVersion         = "9.19.0"
   lazy val hmrcMongoVersion         = "2.7.0"
   lazy val applicationEventVersion  = "0.81.0" // Ensure this version of the application-events library uses the appDomainVersion below
   lazy val applicationDomainVersion = "0.79.0"
