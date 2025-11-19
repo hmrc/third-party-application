2a17d467de1156b8e4057e97e1eebc41c864bb13
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-11-19 15:07:49 +0000

    API-8766 - Support AdminUserIdQP (#583)

--- test/uk/gov/hmrc/thirdpartyapplication/controllers/query/ParamsValidatorSpec.scala
+++ test/uk/gov/hmrc/thirdpartyapplication/controllers/query/ParamsValidatorSpec.scala
@@ -17,17 +17,12 @@
 package uk.gov.hmrc.thirdpartyapplication.controllers.query
 
 import cats.data.NonEmptyList
-import cats.data.Validated.Invalid
-import cats.syntax.validated._
 import org.scalatest.EitherValues
-import org.scalatest.compatible.Assertion
 
 import uk.gov.hmrc.apiplatform.modules.common.domain.models.{ApiIdentifierFixtures, Environment}
 import uk.gov.hmrc.apiplatform.modules.common.utils.{FixedClock, HmrcSpec}
-import uk.gov.hmrc.apiplatform.modules.applications.core.domain.models.{ApplicationWithCollaboratorsFixtures, State}
-import uk.gov.hmrc.apiplatform.modules.applications.query._
+import uk.gov.hmrc.apiplatform.modules.applications.core.domain.models.ApplicationWithCollaboratorsFixtures
 import uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.Param._
-import uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.{NonUniqueFilterParam, Param, UniqueFilterParam}
 
 class ParamsValidatorSpec
     extends HmrcSpec
@@ -35,157 +30,6 @@ class ParamsValidatorSpec
     with ApiIdentifierFixtures
     with EitherValues
     with FixedClock {
-
-  val Pass = ().validNel
-
-  def shouldFail[T](testThis: ErrorsOr[T]): Assertion = {
-    inside(testThis) {
-      case Invalid(_) => succeed
-      case _          => fail("valid when expecting invalid")
-    }
-  }
-
-  "checWants" should {
-    val test: (List[Param[_]]) => ErrorsOr[Unit] = (ps) => ParamsValidator.validateParamCombinations(ps)
-    "disallow wantSubscriptionFields with anything other than a single app query" in {
-      test(List(WantSubscriptionFieldsQP, PageNbrQP(1))) should not be Pass
-      test(List(WantSubscriptionFieldsQP, UserIdQP(userIdOne))) should not be Pass
-      test(List(WantSubscriptionFieldsQP, ApplicationIdQP(applicationIdOne))) shouldBe Pass
-      test(List(WantSubscriptionFieldsQP, ClientIdQP(clientIdOne))) shouldBe Pass
-      test(List(WantSubscriptionFieldsQP, ServerTokenQP("abc"))) shouldBe Pass
-    }
-  }
-
-  "checkLastUsedParamsCombinations" should {
-
-    val test = (ps: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkLastUsedParamsCombinations(ps)
-
-    "pass when provided no last active dates" in {
-      test(List.empty) shouldBe Pass
-    }
-
-    "pass when provided a last active before date" in {
-      test(List(LastUsedAfterQP(instant))) shouldBe Pass
-    }
-
-    "pass when provided a last active after date" in {
-      test(List(LastUsedAfterQP(instant))) shouldBe Pass
-    }
-
-    "pass when provided sensible last active before and last active after dates" in {
-      test(List(LastUsedAfterQP(instant.minusSeconds(5)), LastUsedBeforeQP(instant))) shouldBe Pass
-    }
-
-    "fail when provided incorrect last active before and last active after dates" in {
-      test(List(LastUsedAfterQP(instant), LastUsedBeforeQP(instant.minusSeconds(5)))) shouldBe "Cannot query for used after date that is after a given before date".invalidNel
-    }
-  }
-
-  "checkSubscriptionsParamsCombinations" should {
-    val test: List[NonUniqueFilterParam[_]] => ErrorsOr[Unit] = (ps) => ParamsValidator.checkSubscriptionsParamsCombinations(ps)
-    val Context                                               = ApiContextQP(apiContextOne)
-    val Version                                               = ApiVersionNbrQP(apiVersionNbrOne)
-
-    "pass when given valid combinations" in {
-      test(List(NoSubscriptionsQP)) shouldBe Pass
-      test(List(HasSubscriptionsQP)) shouldBe Pass
-      test(List(Context)) shouldBe Pass
-      test(List(Context, Version)) shouldBe Pass
-      test(List.empty) shouldBe Pass
-    }
-
-    "fail when given invalid combinations" in {
-      shouldFail(test(List(NoSubscriptionsQP, HasSubscriptionsQP)))
-      shouldFail(test(List(NoSubscriptionsQP, Context)))
-      shouldFail(test(List(NoSubscriptionsQP, Context, Version)))
-      shouldFail(test(List(NoSubscriptionsQP, Version)))
-      shouldFail(test(List(HasSubscriptionsQP, Context)))
-      shouldFail(test(List(HasSubscriptionsQP, Context, Version)))
-      shouldFail(test(List(HasSubscriptionsQP, Version)))
-      shouldFail(test(List(Version)))
-    }
-  }
-
-  "checkUniqueParamsCombinations" should {
-    val testBadCombo  = (us: NonEmptyList[UniqueFilterParam[_]], os: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkUniqueParamsCombinations(us, os) should not be Pass
-    val testGoodCombo = (us: NonEmptyList[UniqueFilterParam[_]], os: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkUniqueParamsCombinations(us, os) shouldBe Pass
-
-    "pass when given a correct applicationId" in {
-      testGoodCombo(NonEmptyList.of(ApplicationIdQP(applicationIdOne)), List.empty)
-    }
-
-    "pass when given a correct clientId" in {
-      testGoodCombo(NonEmptyList.of(ClientIdQP(clientIdOne)), List.empty)
-    }
-
-    "pass when given a correct clientId and User Agent" in {
-      testGoodCombo(NonEmptyList.of(ClientIdQP(clientIdOne)), List(ApiGatewayUserAgentQP))
-      testGoodCombo(NonEmptyList.of(ClientIdQP(clientIdOne)), List(GenericUserAgentQP("Bob")))
-    }
-
-    "pass when given a correct serverToken" in {
-      testGoodCombo(NonEmptyList.of(ServerTokenQP("abc")), List.empty)
-    }
-
-    "pass when given a correct serverToken and User Agent" in {
-      testGoodCombo(NonEmptyList.of(ServerTokenQP("abc")), List(ApiGatewayUserAgentQP))
-      testGoodCombo(NonEmptyList.of(ServerTokenQP("abc")), List(GenericUserAgentQP("Bob")))
-    }
-
-    "pass when given a correct applicationId and some irrelevant header" in {
-      testGoodCombo(NonEmptyList.of(ApplicationIdQP(applicationIdOne)), List(GenericUserAgentQP("XYZ")))
-    }
-
-    "fail when mixing two ids" in {
-      testBadCombo(NonEmptyList.of(ApplicationIdQP(applicationIdOne), ClientIdQP(clientIdOne)), List.empty)
-      testBadCombo(NonEmptyList.of(ApplicationIdQP(applicationIdOne), ServerTokenQP("ABC")), List.empty)
-      testBadCombo(NonEmptyList.of(ClientIdQP(clientIdOne), ServerTokenQP("ABC")), List.empty)
-    }
-  }
-
-  "checkVerificationCodeUsesDeleteExclusion" should {
-    val testBadCombo  = (ps: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkVerificationCodeUsesDeleteExclusion(ps) should not be Pass
-    val testGoodCombo = (ps: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkVerificationCodeUsesDeleteExclusion(ps) shouldBe Pass
-
-    "pass when given a verification code and exclude deleted" in {
-      testGoodCombo(List(ExcludeDeletedQP, VerificationCodeQP("ABC")))
-    }
-    "fail when given a verification code only" in {
-      testBadCombo(List(VerificationCodeQP("ABC")))
-    }
-    "fail when given a verification code and a state filter" in {
-      testBadCombo(List(ActiveStateQP, VerificationCodeQP("ABC")))
-    }
-  }
-
-  "checkAppStateFilters" should {
-    val oneState     = MatchOneStateQP(State.PRODUCTION)
-    val manyState    = MatchManyStatesQP(NonEmptyList.of(State.PRODUCTION, State.PRE_PRODUCTION))
-    val blockedState = BlockedStateQP
-    val dateBefore   = uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.Param.AppStateBeforeDateQP(instant)
-
-    val testBadCombo  = (ps: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkAppStateFilters(ps) should not be Pass
-    val testGoodCombo = (ps: List[NonUniqueFilterParam[_]]) => ParamsValidator.checkAppStateFilters(ps) shouldBe Pass
-
-    "pass when only a state filter" in {
-      testGoodCombo(oneState :: Nil)
-      testGoodCombo(manyState :: Nil)
-      testGoodCombo(blockedState :: Nil)
-    }
-
-    "pass when one state with a date before" in {
-      testGoodCombo(oneState :: dateBefore :: Nil)
-    }
-
-    "fail when no state but with a date before" in {
-      testBadCombo(dateBefore :: Nil)
-    }
-
-    "fail when inappropriate state with a date before" in {
-      testBadCombo(manyState :: dateBefore :: Nil)
-      testBadCombo(blockedState :: dateBefore :: Nil)
-    }
-  }
 
   "parseAndValidateParams" should {
     val appOneParam      = "applicationId"   -> Seq(applicationIdOne.toString)
--- test/uk/gov/hmrc/thirdpartyapplication/services/ApplicationServiceSpec.scala
+++ test/uk/gov/hmrc/thirdpartyapplication/services/ApplicationServiceSpec.scala
@@ -48,7 +48,7 @@ import uk.gov.hmrc.apiplatform.modules.applications.core.interface.models.{
   StandardAccessDataToCopy
 }
 import uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.ApplicationQuery
-import uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.Param.UserIdsQP
+import uk.gov.hmrc.apiplatform.modules.applications.query.domain.models.Param.{ExcludeDeletedQP, UserIdsQP}
 import uk.gov.hmrc.apiplatform.modules.commands.applications.domain.models.ApplicationCommands.UpdateLoginRedirectUris
 import uk.gov.hmrc.apiplatform.modules.submissions.mocks.SubmissionsServiceMockModule
 import uk.gov.hmrc.apiplatform.modules.subscriptionfields.mocks.ApiSubscriptionFieldsConnectorMockModule
@@ -582,8 +582,8 @@ class ApplicationServiceSpec
 
   "fetchAllForCollaborators" should {
     "fetch all applications for two given collaborator user ids" in new Setup {
-      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne)))), standardApp)
-      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdTwo)))), standardApp2)
+      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne)), ExcludeDeletedQP)), standardApp)
+      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdTwo)), ExcludeDeletedQP)), standardApp2)
 
       val result = await(underTest.fetchAllForCollaborators(List(userIdOne, userIdTwo), 1))
       result should contain theSameElementsAs List(standardApp, standardApp2)
@@ -591,7 +591,7 @@ class ApplicationServiceSpec
 
     "fetch all applications for two given collaborator user ids in one batch" in new Setup {
       QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(
-        ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne, userIdTwo)))),
+        ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne, userIdTwo)), ExcludeDeletedQP)),
         standardApp,
         standardApp2
       )
@@ -601,8 +601,12 @@ class ApplicationServiceSpec
     }
 
     "deduplicate applications if more than one user belongs to the same application" in new Setup {
-      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne)))), standardApp)
-      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdTwo)))), standardApp, standardApp2)
+      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdOne)), ExcludeDeletedQP)), standardApp)
+      QueryServiceMock.FetchApplicationsByQuery.thenReturnsFor(
+        ApplicationQuery.GeneralOpenEndedApplicationQuery(List(UserIdsQP(List(userIdTwo)), ExcludeDeletedQP)),
+        standardApp,
+        standardApp2
+      )
 
       val result = await(underTest.fetchAllForCollaborators(List(userIdOne, userIdTwo), 1))
       result should contain theSameElementsAs List(standardApp, standardApp2)
