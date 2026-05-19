6826df50b58bd2f1f2d6429716753c712638868e
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-05-19 12:01:23 +0100

    API-9169 - Use branch release domain lib

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   lazy val bootstrapVersion         = "10.7.0"
   lazy val hmrcMongoVersion         = "2.12.0"
   lazy val applicationEventVersion  = "1.1.0" // Ensure this version of the application-events library uses the appDomainVersion below
-  lazy val applicationDomainVersion = "1.2.0-SNAPSHOT"
+  lazy val applicationDomainVersion = "1.1.1"
 
   private lazy val compileDeps = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"                % bootstrapVersion,
